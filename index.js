8418b6bc36bc07f0d3e90e1dff31510f07b57cfd
Jarrett Delaney <jarrettdelaney1@gmail.com>
2024-02-29 20:40:31 -0800

    created JS variables.

---
+++ index.js
@@ -0,0 +1,10 @@
+const startButton = document.getElementById('start');
+const questionContainer = document.getElementById('question-container');
+const question = document.getElementById('question');
+const answerButton = document.getElementById('answer-btn');
+const endSlide = document.getElementById('end-slide');
+const score = document.getElementById('final-score');
+const initialsPlaceholder = document.getElementById('initials');
+const submit = document.getElementById('submit-score');
+
+
